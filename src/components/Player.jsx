71ff488236594cc1f31130cb674728cb7414aaed
Stephen Kettley <kettleystephen@gmail.com>
2023-11-23 17:19:25 +0200

    update user input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,24 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null)
+
+
+  const handleClick = (event) => {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = ""; // you want to avoid using refs to manipulate the DOM (goes against declarative idea)
+  }
   return (
+    <>
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { enteredPlayerName ?? "Unknown Player"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
+    </>
   );
 }
