1e191e1270c153ecba6546dab963ff420dba5375
Stephen Kettley <kettleystephen@gmail.com>
2023-11-24 10:08:02 +0200

    introduce portal for result modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
 
 const ResultModal = forwardRef(function ResultModal(props, ref) {
     const dialog = useRef();
@@ -14,7 +16,7 @@ const ResultModal = forwardRef(function ResultModal(props, ref) {
             }
         }
     });
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={props.onReset}>
              {userLost && <h2>You Lost!</h2>}
              {!userLost && <h2>Your Score: {score}</h2>}
@@ -24,7 +26,8 @@ const ResultModal = forwardRef(function ResultModal(props, ref) {
              <form method="dialog" onSubmit={props.onReset}>
                 <button>Close</button>
              </form>
-        </dialog>
+        </dialog>,
+        document.getElementById("modal")
     )
 })
 
