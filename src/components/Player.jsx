7c44237d0166cb36d9e89c18e30cd9d1f31f8daa
Stephen Kettley <kettleystephen@gmail.com>
2023-11-28 18:37:50 +0200

    update background styling

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,22 +3,21 @@ import { useRef, useState } from "react";
 export default function Player() {
   const playerName = useRef();
 
-  const [enteredPlayerName, setEnteredPlayerName] = useState(null)
-
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   const handleClick = (event) => {
     setEnteredPlayerName(playerName.current.value);
     playerName.current.value = ""; // you want to avoid using refs to manipulate the DOM (goes against declarative idea)
-  }
+  };
   return (
     <>
-    <section id="player">
-      <h2>Welcome { enteredPlayerName ?? "Unknown Player"}</h2>
-      <p>
-        <input ref={playerName} type="text"/>
-        <button onClick={handleClick}>Set Name</button>
-      </p>
-    </section>
+      <section id="player">
+        <h2>Welcome {enteredPlayerName ?? "Unknown Player"}</h2>
+        <p>
+          <input ref={playerName} type="text" />
+          <button onClick={handleClick}>Set Name</button>
+        </p>
+      </section>
     </>
   );
 }
